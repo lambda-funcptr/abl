ed8f84a56b2e1cb553d311dd51f378e619d121f5
Guanyu Wu <lambda@funcptr.org>
2023-06-21 00:52:44 -0400

    Make sure we properly build the go binary to dist...

--- go/build.sh
+++ go/build.sh
@@ -2,4 +2,4 @@ export GOPATH=$(readlink -f $(dirname $0))
 
 cd "$GOPATH/funcptr.org/abl/bootmenu"
 
-exec go build -v -a -ldflags "-w -s -extldflags -static" -o /tmp/abl/sbin/bootmenu cmd/bootmenu.go
+exec go build -v -a -ldflags "-w -s -extldflags -static" -o ../../../../dist/bootmenu cmd/bootmenu.go
