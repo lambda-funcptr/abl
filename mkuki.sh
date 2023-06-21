2429d9797c5aadc2878fee6c0481e7b5f500cec7
Guanyu Wu <lambda@funcptr.org>
2023-06-21 00:53:27 -0400

    Inject the commandline into the UKI

--- mkuki.sh
+++ mkuki.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 echo ">>> Generating UKI (Generic)"
-efi-mkuki -o /output/abl.efi /output/linux/arch/x86_64/boot/bzImage /output/initramfs.zstd
+efi-mkuki -c /config/cmdline -o /output/abl.efi /output/linux/arch/x86_64/boot/bzImage /output/initramfs.zstd
 
 for ucode_arch in amd-ucode intel-ucode; do
   echo ">>> Generating UKI (${ucode_arch})"
-  efi-mkuki -o /output/abl.${ucode_arch}.efi /output/linux/arch/x86_64/boot/bzImage /boot/${ucode_arch}.img /output/initramfs.zstd
+  efi-mkuki -c /config/cmdline -o /output/abl.${ucode_arch}.efi /output/linux/arch/x86_64/boot/bzImage /boot/${ucode_arch}.img /output/initramfs.zstd
 done
