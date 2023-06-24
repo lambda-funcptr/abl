25eb8a063616044f4c3b0c5d02fe8212d3629e8b
Guanyu Wu <lambda@funcptr.org>
2023-06-23 23:38:33 -0400

    Make vmesp also load the bootcfg f2fs partition

--- test/vmesp.sh
+++ test/vmesp.sh
@@ -2,4 +2,4 @@
 
 workdir=$(git rev-parse --show-toplevel)
 
-qemu-system-x86_64 -smbios type=0,uefi=on -bios /usr/share/ovmf/x64/OVMF.fd -enable-kvm -m 4G ${workdir}/test/images/esp.fat
+qemu-system-x86_64 -smbios type=0,uefi=on -bios /usr/share/ovmf/x64/OVMF.fd -enable-kvm -m 4G -drive file=${workdir}/test/images/esp.fat -drive file=${workdir}/test/images/bootcfg.f2fs
